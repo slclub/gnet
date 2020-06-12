f1df588869fef31bb0f3b1a08925b30e52c858b4
slclub@163.com <slclub@163.com>
2020-06-12 17:04:59 +0800

    add an middle code

--- defined/code.go
+++ defined/code.go
@@ -5,4 +5,8 @@ const (
 	CODE_JUMP_CURRENT_NODE = -91001
 	// jump current goroutine.
 	CODE_JUMP_CURRENT_FLOW = -91002
+
+	//gcore.Middle
+	// empty handle used.
+	CODE_NOT_ALLOWED_EMPTY_HANDLE = -91010
 )
