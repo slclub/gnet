421f3bc06568e42d9151c71766e42640d108da35
slclub@163.com <slclub@163.com>
2020-06-17 11:21:41 +0800

    jwt login

--- defined/code.go
+++ defined/code.go
@@ -1,6 +1,14 @@
 package defined
 
 const (
+	// success code
+	SUCCESS = 0
+	// jwt login
+	ERR_AUTH_GENERATE              = -200001
+	ERR_AUTH                       = -200002
+	ERROR_AUTH_CHECK_TOKEN_FAIL    = -200003
+	ERROR_AUTH_CHECK_TOKEN_TIMEOUT = -200004
+
 	// jump current process node.
 	CODE_JUMP_CURRENT_NODE = -91001
 	// jump current goroutine.
